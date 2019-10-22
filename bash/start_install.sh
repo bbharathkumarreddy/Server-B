031d64424a3d8765df77c589d1ed121080cd992f
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-10-22 22:20:22 +0530

    dev-z1

--- bash/start_install.sh
+++ bash/start_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #install command below
-#sudo apt-get install git -y && sudo mkdir -p /var/www && sudo mkdir -p /var/www/www-panel && sudo git clone -b dev-z1 https://bbharathkumarreddy:bvsschool2019@github.com/bbharathkumarreddy/admin-b-panel.git /var/www/www-panel/ && sudo bash /var/www/www-panel/start_install.sh
+#sudo apt-get install git -y && sudo mkdir -p /var/www && sudo mkdir -p /var/www/www-panel && sudo git clone -b dev-z1 https://bbharathkumarreddy:bvsschool2019@github.com/bbharathkumarreddy/server-b.git /var/www/www-panel/ && sudo bash /var/www/www-panel/start_install.sh
 
 #/etc/ssh/sshd_config
 #/etc/php/7.2/fpm/php.ini
