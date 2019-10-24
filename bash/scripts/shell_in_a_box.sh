3b6b9838880b1c1424d21d922d240e8a1e2da59f
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-10-25 00:03:17 +0530

    dev-z1

--- bash/scripts/shell_in_a_box.sh
+++ bash/scripts/shell_in_a_box.sh
@@ -5,7 +5,8 @@ echo -------------------------------------------------
 #sudo apt-cache search shellinabox
 #sudo apt-get install openssl shellinabox -y
 sleep 2
-sed -i "s/4200/4201/g" /etc/default/shellinabox
+echo $1 > /var/www/server-b-data/shell_in_a_box_port
+sed -i "s/4200/$1/g" /etc/default/shellinabox
 sed -i "s/--no-beep/--no-beep   --disable-ssl/g" /etc/default/shellinabox
 sleep 1
 sudo service shellinabox stop
