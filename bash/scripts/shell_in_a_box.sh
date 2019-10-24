024d4de6f996d6b34cfa9690b37908e8145772c3
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-10-25 00:12:02 +0530

    dev-z1

--- bash/scripts/shell_in_a_box.sh
+++ bash/scripts/shell_in_a_box.sh
@@ -2,8 +2,8 @@
 echo -------------------------------------------------
 echo ++++++++++++  SHELL IN A BOX    +++++++++++++++++
 echo -------------------------------------------------
-#sudo apt-cache search shellinabox
-#sudo apt-get install openssl shellinabox -y
+sudo apt-cache search shellinabox
+sudo apt-get install openssl shellinabox -y
 sleep 2
 echo $1 > /var/www/server-b-data/shell_in_a_box_port
 sed -i "s/4200/$1/g" /etc/default/shellinabox
