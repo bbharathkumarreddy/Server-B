ffafef71d0f24c47ec8de60f308d0f1ae5aae13e
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-10-25 00:35:20 +0530

    dev-z1

--- run/app/ssh.php
+++ run/app/ssh.php
@@ -1,8 +1,8 @@
 <?php include_once('top.php'); ?>
 <div class="container-fluid">
 <?php $public_ip = readfile("/var/www/server-b-data/server_public_ip"); ?>
-<?php $server_b_port = readfile("/var/www/server-b-data/server_b_port"); ?>
-<iframe src="http://<?php readfile("/var/www/server-b-data/server_public_ip").':'.readfile("/var/www/server-b-data/server_b_port"); ?>/" frameborder="0" style="width:100%;height:600px;"></iframe>
+<?php $shell_in_a_box_port = readfile("/var/www/server-b-data/shell_in_a_box_port"); ?>
+<iframe src="http://<?php readfile("/var/www/server-b-data/server_public_ip").':'.readfile("/var/www/server-b-data/shell_in_a_box_port"); ?>/" frameborder="0" style="width:100%;height:600px;"></iframe>
 
 </div>
 <?php include_once('bottom.php'); ?>
