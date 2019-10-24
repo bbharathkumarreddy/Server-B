c0e1e15460db1a33b4360b08e2cbf35b7a8ec982
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-10-25 00:42:31 +0530

    dev-z1

--- bash/scripts/newuser-it.sh
+++ bash/scripts/newuser-it.sh
@@ -2,4 +2,4 @@
 read -p "Create new ubunut linux user : " shell_in_a_box_user
 read -p "Set password : " shell_in_a_box_pwd
 
-sudo bash  /var/www/server-b/bash/scripts/newuser shell_in_a_box_user shell_in_a_box_pwd
+sudo bash  /var/www/server-b/bash/scripts/newuser.sh shell_in_a_box_user shell_in_a_box_pwd
