b56b4b1d06e2d8a1baaab1851e5fc14fb989cab8
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-10-26 23:16:48 +0530

    dev-z1

--- bash/scripts/ip-save.sh
+++ bash/scripts/ip-save.sh
@@ -2,7 +2,5 @@
 server_ip=$(curl ifconfig.co)
 echo $server_ip > /var/www/server-b-data/server_public_ip
 
-server_int_ip=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+server_int_ip=hostname -I
 echo $server_int_ip > /var/www/server-b-data/server_int_ip
-
-echo 'userconnect1' > /s.txt 
