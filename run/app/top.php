12d14f4df900536f9d9048679a3d01ed068a6fc5
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-10-27 00:33:03 +0530

    dev-z1

--- run/app/top.php
+++ run/app/top.php
@@ -1,5 +1,5 @@
 <?php $host = "http://".file_get_contents("/var/www/server-b-data/server_public_ip").`:`.file_get_contents("/var/www/server-b-data/server_b_port"); ?>
-<? echo 'Link - '.$host; ?>
+<?php echo 'Link - '.$host; ?>
 <!DOCTYPE html>
 <html lang="en">
 
