d8bc91d3012e6b3b91a03ffbb57445bb0b914754
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-10-28 02:46:36 +0530

    dev-z1

--- run/api/sys_stat.php
+++ run/api/sys_stat.php
@@ -1,1 +1,5 @@
-<?php echo shell_exec('tail -30 /var/www/server-b-data/sys_stat_log'); ?>
+<?php 
+$data  = shell_exec('tail -30 /var/www/server-b-data/sys_stat_log');
+$data = substr($data, 0, -1);
+echo '{"data":'.$data.'}'; 
+?>
