1d4ecc482de26bca63be333e1ead925449cc1c95
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-10-27 12:55:08 +0530

    dev-z1

--- run/api/update-server-b.php
+++ run/api/update-server-b.php
@@ -1,6 +1,6 @@
 <?php
 echo 'git update started';
-$a = exec("sudo bash /var/www/server-b/scripts/server-b-git-update.sh");
+$a = exec("sudo bash /var/www/server-b/bash/scripts/server-b-git-update.sh");
 echo $a;
 echo 'complete';
 ?>
