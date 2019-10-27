49a6dfd660f001d6d8f70ae1aa6dc1ebd5a802b6
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-10-27 13:30:35 +0530

    dev-z1

--- run/api/update-server-b.php
+++ run/api/update-server-b.php
@@ -1,10 +1,10 @@
 <?php
 echo 'git update started<br>';
-echo shell_exec("cd /var/www/server-b/ && sudo git stash");
-echo shell_exec("cd /var/www/server-b/ && sudo git reset");
-echo shell_exec("cd /var/www/server-b/ && sudo git pull origin dev-z1");
-echo shell_exec("sudo chmod -R 777 /var/www/server-b");
-echo shell_exec("sudo chmod -R 777 /var/www/server-b-data");
+echo shell_exec("cd /var/www/server-b/ && git stash");
+echo shell_exec("cd /var/www/server-b/ && git reset");
+echo shell_exec("cd /var/www/server-b/ && git pull origin dev-z1");
+echo shell_exec("chmod -R 777 /var/www/server-b");
+echo shell_exec("chmod -R 777 /var/www/server-b-data");
 echo shell_exec('ls'); 
 echo '<br>complete';
 ?>
