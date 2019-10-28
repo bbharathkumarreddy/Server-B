05d66c802bfed38c9d000fd2fbb8df994cf6aa70
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-10-28 21:52:19 +0530

    dev-z1

--- run/api/sys_command.php
+++ run/api/sys_command.php
@@ -2,11 +2,11 @@
 echo 'user-'.shell_exec('whoami');
 $status = 'fail';
 if(isset($_GET['cmd']) && $_GET['cmd'] == 'shutdown'){ 
-    $data = shell_exec("/bin/shutdown");
+    $data = shell_exec("sudo /bin/shutdown");
     $status = 'success';
 }
 elseif(isset($_GET['cmd']) && $_GET['cmd'] == 'reboot'){ 
-    $data = shell_exec("bash /var/www/server-b/bash/scripts/reboot.sh");
+    $data = shell_exec("sudo reboot");
     $status = 'success';
 }
 echo $status.'<br>'.$data;
