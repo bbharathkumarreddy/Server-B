3bddb0a409977087a38da1ab7739aee958213288
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-10-28 16:52:11 +0530

    dev-z1

--- run/api/sys_command.php
+++ run/api/sys_command.php
@@ -1,11 +1,11 @@
 <?php
 $status = 'fail';
 if(isset($_GET['cmd']) && $_GET['cmd'] == 'shutdown'){ 
-    $data = shell_exec('shutdown');
+    $data = shell_exec('sudo shutdown');
     $status = 'success';
 }
 elseif(isset($_GET['cmd']) && $_GET['cmd'] == 'reboot'){ 
-    $data = shell_exec('reboot');
+    $data = shell_exec('sudo reboot');
     $status = 'success';
 }
 echo $status.'<br>'.$data;
