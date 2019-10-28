3e9db8d90c1f8a7ba5a8a538612360210521aa65
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-10-28 17:16:12 +0530

    dev-z1

--- run/api/sys_command.php
+++ run/api/sys_command.php
@@ -1,11 +1,11 @@
 <?php
 $status = 'fail';
 if(isset($_GET['cmd']) && $_GET['cmd'] == 'shutdown'){ 
-    $data = shell_exec('shutdown');
+    $data = shell_exec("bash /var/www/server-b/bash/scripts/shutdown.sh");
     $status = 'success';
 }
 elseif(isset($_GET['cmd']) && $_GET['cmd'] == 'reboot'){ 
-    $data = shell_exec('reboot');
+    $data = shell_exec("bash /var/www/server-b/bash/scripts/reboot.sh");
     $status = 'success';
 }
 echo $status.'<br>'.$data;
