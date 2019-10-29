d6586f0548602ec379c6aebfd9c5209d2a218a34
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-10-29 21:21:32 +0530

    dev-z1

--- run/api/sys_stat.php
+++ run/api/sys_stat.php
@@ -1,6 +1,8 @@
 <?php 
 $data = shell_exec('tail -13 /var/www/server-b-data/sys_stat_log');
 $data = str_replace("'", '"',$data);
-$data = rtrim($data,', ');
-echo '{"data":['.$data.']}'; 
+$data = str_replace("], ]", ']]',$data);
+
+$new_json =  '{"data":['.$data.']}'; 
+echo json_encode($new_json);
 ?>
