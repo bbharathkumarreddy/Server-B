12916a7d4f143f47b4d9183cc5bc6656f417b3e9
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-10-29 21:16:44 +0530

    dev-z1

--- run/api/sys_stat.php
+++ run/api/sys_stat.php
@@ -3,5 +3,5 @@ $data = shell_exec('tail -13 /var/www/server-b-data/sys_stat_log');
 $data = str_replace("'", '"',$data);
 $data = rtrim($data,',');
 
-echo '{"data":['.$data.']}'; 
+echo '{"data":[1,2,3]}'; 
 ?>
