f6e5e432d2193d792bd3bb4e4f8d0802de3f06c4
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-10-29 21:53:33 +0530

    dev-z1

--- run/app/dashboard.php
+++ run/app/dashboard.php
@@ -186,7 +186,7 @@
     <script src="vendor/chart.js/Chart.min.js"></script>
 
     <!-- Page level custom scripts -->
-    <script src="js/demo/chart-area-demo.js"></script>
+    <!-- <script src="js/demo/chart-area-demo.js"></script> -->
     <script src="js/demo/chart-pie-demo.js"></script>
     <script src="js/demo/line-chart.js"></script>
     <script>
