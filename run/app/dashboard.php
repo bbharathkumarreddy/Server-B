73f9ce4f37ef0039cc96123b9deaaaef9a536db3
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-11-02 18:13:16 +0530

    dev-z1

--- run/app/dashboard.php
+++ run/app/dashboard.php
@@ -120,7 +120,7 @@
             </div>
         </div>
         <div class="col-6 col-lg-6">
-            <div class="card shadow mb-4" style="height:550px;">
+            <div class="card shadow mb-4" style="height:750px;">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                     <h6 class="m-0 font-weight-bold text-primary">System Monitoring</h6>
@@ -138,7 +138,7 @@
             </div>
         </div>
         <div class="col-6 col-lg-6">
-            <div class="card shadow mb-4" style="height:550px;">
+            <div class="card shadow mb-4" style="height:750px;">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                     <h6 class="m-0 font-weight-bold text-primary">Newtwork & Disk Analytics</h6>
