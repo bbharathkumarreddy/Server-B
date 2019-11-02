150dffc4e0d1c9aaa150ed8f26c48b38deb87b5e
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-11-02 18:01:36 +0530

    dev-z1

--- run/app/dashboard.php
+++ run/app/dashboard.php
@@ -120,7 +120,7 @@
             </div>
         </div>
         <div class="col-6 col-lg-6">
-            <div class="card shadow mb-4">
+            <div class="card shadow mb-4" style="height:550px;">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                     <h6 class="m-0 font-weight-bold text-primary">System Monitoring</h6>
