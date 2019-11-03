b4d04f2b3ba2b13b54da793d2b2b8a57a8594edb
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-11-03 14:28:24 +0530

    dev-z1

--- run/app/top.php
+++ run/app/top.php
@@ -167,7 +167,7 @@ $host = str_replace("\n", "", $host); ?>
             <div id="content">
 
                 <!-- Topbar -->
-                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                <nav class="navbar navbar-expand navbar-collapse collpase navbar-light bg-white topbar mb-4 static-top shadow">
 
                     <!-- Sidebar Toggle (Topbar) -->
                     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
