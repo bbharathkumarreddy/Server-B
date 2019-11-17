2a7ede31092673c8ce510b3f1383d9b30c234757
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-11-17 23:32:45 +0530

    start

--- run/app/dashboard.php
+++ run/app/dashboard.php
@@ -14,7 +14,7 @@
                 <!-- Card Body -->
                 <div class="card-body">
                     <div class="chart-pie pt-4 pb-2" style="height: 150px !important;">
-                        <canvid id="pie_chart_1"></canvas>
+                        <canvas id="pie_chart_1"></canvas>
                     </div>
                     <div class="mt-4 text-center small">
                         <span class="mr-2">
