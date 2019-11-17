c546e0ef975581bdaffe478b569cbe75db014d2a
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-11-17 22:49:38 +0530

    start

--- run/app/dashboard.php
+++ run/app/dashboard.php
@@ -104,7 +104,7 @@
             <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
+                    <h6 class="m-0 font-weight-bold text-primary">Disk Storage</h6>
                 </div>
                 <!-- Card Body -->
                 <div class="card-body">
@@ -113,7 +113,7 @@
                     </div>
                     <div class="mt-4 text-center small">
                         <span class="mr-2">
-                            <i class="fas fa-circle text-success"></i> Data Upload
+                            <i class="fas fa-circle text-success"></i> Disk Storage
                         </span>
                     </div>
                 </div>
