20af251ef27ac6f435a0a2fc4f94350f30b1c009
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-11-17 23:11:14 +0530

    start

--- run/api/sys_stat.php
+++ run/api/sys_stat.php
@@ -1,26 +1,7 @@
 <?php 
 if(isset($_GET['live']) && $_GET['live'] == 'true')
 {   echo 'a';
-    $LOAD_1 = shell_exec(`free -m`);
- /*   $LOAD_1 = shell_exec("top -bn1 | grep load | awk '{printf "%s\n", $11}");
-    LOAD_1=$(top -bn1 | grep load | awk '{printf "%s\n", $10}')
-LOAD_5=$(top -bn1 | grep load | awk '{printf "%s\n", $11}')
-LOAD_15=$(top -bn1 | grep load | awk '{printf "%s\n", $12}')
-CPU=$(top -bn1 | grep load | awk '{printf "%.2f\n", $(NF-2)}')
-DISK_USAGE=$(df -h | awk '$NF=="/"{printf "%.1f\n", $3}')
-DISK_TOTAL=$(df -h | awk '$NF=="/"{printf "%.1f\n", $2}')
-MEM_USAGE=$(free -m | awk 'NR==2{printf "%s\n", $3}')
-MEM_TOTAL=$(free -m | awk 'NR==2{printf "%s\n", $2}')
-TIMESTAMP=$(date +%s)
-
-LINE=`grep ens4 /proc/net/dev | sed s/.*://`;
-NET_RECEIVED=`echo $LINE | awk '{print $1}'`
-NET_TRANSMITTED=`echo $LINE | awk '{print $9}'`
-
-NET_RECEIVED=$(($NET_RECEIVED/1000))
-
-NET_TRANSMITTED=$(($NET_TRANSMITTED/1000))*/
-
+    $LOAD_1 = shell_exec('free -m');
 
      echo $LOAD_1;
 }
