693a2ca0bfd154bb04cb053f61f51979a827cd02
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-11-17 23:12:26 +0530

    start

--- run/api/sys_stat.php
+++ run/api/sys_stat.php
@@ -1,7 +1,7 @@
 <?php 
 if(isset($_GET['live']) && $_GET['live'] == 'true')
 {   echo 'a';
-    $LOAD_1 = shell_exec('free -m');
+    $LOAD_1 = get_server_memory_usage();
 
      echo $LOAD_1;
 }
@@ -12,5 +12,18 @@ $template = '[TIMESTAMP,CPU,LOAD_1,LOAD_5,LOAD_15,DISK_USAGE,DISK_TOTAL,MEM_USAG
 
 $new_json =  '{"data":['.$data.']}';
 echo str_replace("],\n]", ']]',$new_json); 
+}
+
+function get_server_memory_usage(){
+
+    $free = shell_exec('free');
+    $free = (string)trim($free);
+    $free_arr = explode("\n", $free);
+    $mem = explode(" ", $free_arr[1]);
+    $mem = array_filter($mem);
+    $mem = array_merge($mem);
+    $memory_usage = $mem[2]/$mem[1]*100;
+
+    return $memory_usage;
 }
 ?>
