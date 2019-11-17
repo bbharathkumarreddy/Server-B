aefd957501ed4a598384307eca3c71d4727c9a56
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-11-17 23:48:57 +0530

    start

--- run/api/sys_stat.php
+++ run/api/sys_stat.php
@@ -9,6 +9,7 @@ $data = str_replace("'", '"',$data);
 $template = '[TIMESTAMP,CPU,LOAD_1,LOAD_5,LOAD_15,DISK_USAGE,DISK_TOTAL,MEM_USAGE,MEM_TOTAL,NET_RECEIVED,NET_TRANSMITTED,NET_TOTAL]';
 
 $new_json =  '{"data":['.$data.']}';
+header('Content-Type: application/json');
 echo str_replace("],\n]", ']]',$new_json); 
 }
 
