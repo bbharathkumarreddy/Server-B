0821a97e1dd238801a45cf9fd4d83c96c13a27c4
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-11-17 13:12:27 +0530

    start

--- run/app/filemanager.php
+++ run/app/filemanager.php
@@ -1,8 +1,5 @@
 <?php include_once('top.php'); ?>
 <div class="container-fluid">
-<?php $public_ip = readfile("/var/www/server-b-data/server_public_ip"); ?>
-<?php $server_b_port = readfile("/var/www/server-b-data/server_b_port"); ?>
-<iframe src="http://<?php readfile("/var/www/server-b-data/server_public_ip");?>:<?php readfile("/var/www/server-b-data/server_b_port"); ?>/app/tinyfilemanager.php?p=" frameborder="0" style="width:100%;height:1000px;"></iframe>
 </div>
 <?php include_once('bottom.php'); ?>
 <!-- Page level plugins -->
