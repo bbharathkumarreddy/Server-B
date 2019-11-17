921c2a50bf2677cf95961b8e20c6f5aa2c28ecd7
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-11-17 23:13:21 +0530

    start

--- run/api/sys_stat.php
+++ run/api/sys_stat.php
@@ -16,14 +16,9 @@ echo str_replace("],\n]", ']]',$new_json);
 
 function get_server_memory_usage(){
 
-    $free = shell_exec('free');
-    $free = (string)trim($free);
-    $free_arr = explode("\n", $free);
-    $mem = explode(" ", $free_arr[1]);
-    $mem = array_filter($mem);
-    $mem = array_merge($mem);
-    $memory_usage = $mem[2]/$mem[1]*100;
+    $free = shell_exec('free -m');
+  
 
-    return $memory_usage;
+    return $free;
 }
 ?>
