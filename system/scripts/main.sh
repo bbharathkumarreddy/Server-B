e8bf516d33e0c0dd1ddc959f421d043cb9618150
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-12-22 20:18:26 +0530

    dev

--- system/scripts/main.sh
+++ system/scripts/main.sh
@@ -1,5 +1,48 @@
 #!/bin/bash
-source ../config.sh
+source /var/www/system/config.sh
+
+#sudo apt-get install git -y && sudo mkdir -p /var/www && sudo mkdir -p /var/www/server-b && sudo git clone -b dev-z1 https://bbharathkumarreddy:bvsschool2019@github.com/bbharathkumarreddy/server-b.git /var/www/server-b/ && sudo bash /var/www/server-b/bash/start_install.sh
+
+#/etc/ssh/sshd_config
+#/etc/php/7.2/fpm/php.ini
+#/etc/nginx/sites-enabled/default
+#/etc/nginx/sites-enabled/conf
+
+install
+
+install(){
+    echo --------------------------------------------------
+    echo +++++  Server B Installation Started  ++++++
+    echo --------------------------------------------------
+    echo $ssh_port
+
+    sudo mkdir $site_path
+
+    sudo mkdir $site_path'php'
+    sudo mkdir $site_path'node'
+    sudo mkdir $site_path'static'
+    sudo mkdir $site_path'cert'
+
+    server_b_file_per
+
+    load_ip
+    load_os
+    update_upgrade
+    install_nano
+    install_nginx
+    install_php
+    install_mysql mysql123 alt_root mysql123 3305 0.0.0.0
+    install_shell_in_a_box 8887
+    install_node_npm
+    sudo cp /etc/ssh/sshd_config $backup_path`sshd_config_bck`
+    generate_auth_key
+    new_user ubt ubt
+    ssh_port_set 24
+
+    server_b_file_per
+
+    crontab -l | { cat; echo "@reboot $scripts_path'main.sh' load_ip > /dev/null 2>&1"; } | crontab -
+}
 
 getallKey(){
    cat ../config.sh
