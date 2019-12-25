308578fccb541e6a546d577e9f250f57352926ce
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-12-25 20:44:28 +0530

    dev

--- console/api/server-b-update.php
+++ console/api/server-b-update.php
@@ -6,6 +6,6 @@ echo shell_exec("cd /var/www/server-b/ && git pull origin dev-z1");
 echo shell_exec("bash /var/www/server-b/bash/scripts/update-formation.sh");
 echo shell_exec("chmod -R 777 /var/www/server-b");
 echo shell_exec("chmod -R 777 /var/www/server-b-data");
-echo shell_exec("bash /var/www/server-b/system/scripts/service.sh setKey ".date("Y/m/d").' '.date("h:i:sa"));
+echo shell_exec("bash /var/www/server-b/system/scripts/service.sh setKey ".date("Y-m-d").'.'.date("h:i:sa"));
 echo '<br>complete';
 ?>
