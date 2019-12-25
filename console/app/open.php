96db71d390b49a99b1560ee60950c0918cb3b5d2
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-12-26 00:34:16 +0530

    dev

---
+++ console/app/open.php
@@ -0,0 +1,15 @@
+<?php include('top.php'); ?>
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Open</h1>
+    </div>
+
+    <div class="row">
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+<?php include('bottom.php'); ?>
