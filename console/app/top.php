dba66f167ed0ca750fb1aadee38c17696085437f
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-12-26 00:29:07 +0530

    dev

--- console/app/top.php
+++ console/app/top.php
@@ -69,7 +69,7 @@ $public_ip=shell_exec("bash /var/www/server-b/system/scripts/service.sh getKey p
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="dashboard.php">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="far fa-hdd"></i>
                 </div>
