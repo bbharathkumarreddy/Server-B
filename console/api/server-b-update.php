bf574e45b5c33fa56e5357cf689bfe41d3aa6287
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-12-25 20:23:26 +0530

    dev

---
+++ console/api/server-b-update.php
@@ -0,0 +1,10 @@
+<?php
+echo 'git server b update started<br>';
+echo shell_exec("cd /var/www/server-b/ && git stash");
+echo shell_exec("cd /var/www/server-b/ && git reset");
+echo shell_exec("cd /var/www/server-b/ && git pull origin dev-z1");
+echo shell_exec("bash /var/www/server-b/bash/scripts/update-formation.sh");
+echo shell_exec("chmod -R 777 /var/www/server-b");
+echo shell_exec("chmod -R 777 /var/www/server-b-data");
+echo '<br>complete';
+?>
