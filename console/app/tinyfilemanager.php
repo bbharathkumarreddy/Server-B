121061a825e1a3f375ac8d01522106357617a5c2
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-12-28 22:06:09 +0530

    dev

--- console/app/tinyfilemanager.php
+++ console/app/tinyfilemanager.php
@@ -326,7 +326,7 @@ if ($use_auth && isset($_SESSION[FM_SESSION_ID]['logged'])) {
 
 // clean and check $root_path
 $root_path = rtrim($root_path, '\\/');
-$root_path = str_replace('\\', '/', $root_path);
+//$root_path = str_replace('\\', '/', $root_path);
 if (!@is_dir($root_path)) {
     echo "<h1>Root path \"{$root_path}\" not found!</h1>";
     exit;
