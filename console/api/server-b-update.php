61ce4053e8b02bfae2521d2e735b3fda53d7b80d
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-12-29 10:29:41 +0530

    dev

--- console/api/server-b-update.php
+++ console/api/server-b-update.php
@@ -1,11 +1,18 @@
 <?php
-echo 'Server B Update Started<br>';
+echo '-------------------------------------------------------------------------------------<br>';
+echo '+                          Server B Update Started                                   +<br>';
+echo '-------------------------------------------------------------------------------------<br>';
 echo shell_exec("cd /var/www/server-b/ && git stash");
 echo shell_exec("cd /var/www/server-b/ && git reset");
+echo '<br>&nbsp;&nbsp;&nbsp;Note:';
 echo shell_exec("cd /var/www/server-b/ && git pull origin dev-z1");
+echo '<br>';
 echo shell_exec("bash /var/www/server-b/bash/scripts/update-formation.sh");
 echo shell_exec("chmod -R 777 /var/www/server-b");
 echo shell_exec("chmod -R 777 /var/www/server-b-data");
 echo shell_exec("bash /var/www/server-b/system/scripts/service.sh setKey update_date ".date("Y-m-d").'.'.date("h:i:sa"));
 echo '<br>complete';
+echo '-------------------------------------------------------------------------------------<br>';
+echo '+                          Server B Update Completed                                 +<br>';
+echo '-------------------------------------------------------------------------------------<br>';
 ?>
