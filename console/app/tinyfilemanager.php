da8acf597d59a891dbcd5ebc9ae6809e9cf23589
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2019-12-29 12:38:50 +0530

    dev

--- console/app/tinyfilemanager.php
+++ console/app/tinyfilemanager.php
@@ -743,6 +743,35 @@ if (isset($_GET['dl'])) {
         fm_redirect(FM_SELF_URL . '?p=' . urlencode(FM_PATH));
     }
 }
+// Download
+if (isset($_GET['vl'])) {
+    $dl = $_GET['vl'];
+    $dl = fm_clean_path($dl);
+    echo = $dl;
+    exit;
+    $dl = str_replace('/', '', $dl);
+    $path = FM_ROOT_PATH;
+    if (FM_PATH != '') {
+        $path .= '/' . FM_PATH;
+    }
+    if ($dl != '' && is_file($path . '/' . $dl)) {
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="' . basename($path . '/' . $dl) . '"');
+        header('Content-Transfer-Encoding: binary');
+        header('Connection: Keep-Alive');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($path . '/' . $dl));
+        ob_end_clean();
+        readfile($path . '/' . $dl);
+        exit;
+    } else {
+        fm_set_msg('File not found', 'error');
+        fm_redirect(FM_SELF_URL . '?p=' . urlencode(FM_PATH));
+    }
+}
 
 // Upload
 if (!empty($_FILES) && !FM_READONLY) {
