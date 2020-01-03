a2dc79d557a40ba0908694f27409a5589d9e54e4
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2020-01-03 07:53:14 +0530

    dev

--- console/app/dashboard.php
+++ console/app/dashboard.php
@@ -158,6 +158,25 @@
                                 </div>
                                 <hr>';
                                 }
+                                else if(strpos($si, '[ - ]') !== false){
+                                    $status_text='Stopper';
+                                    $status_icon='<div class="col-xs-3 col-md-3 md-3 pointer"><i class="fas fa-circle text-danger"></i>&nbsp;&nbsp;<span>Stopped</span></div>';
+                                    $service_name_text=str_replace('[ - ]','',$i);
+                                    echo '<div class="row">
+                                    '.$status_icon.'
+                                    <span>Running</span></div>
+                                    <div class="col-xs-6 col-md-6 md-6"><span>'.$service_name_text.' Service<span></div>
+                                    <div class="col-xs-3 col-md-3 md-3">
+                                        <div>
+                                            <i class="fas fa-play text-success pointer" title="Start Service"></i> &nbsp; &nbsp;
+                                            <i class="fas fa-stop text-danger pointer" title="Stop Service"></i> &nbsp; &nbsp;
+                                            <i class="fas fa-redo-alt text-info pointer" title="Restart Service"></i> &nbsp; &nbsp;
+                                            <i class="fas fa-info text-primary pointer" title="Service Status"></i> &nbsp; &nbsp;
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr>';
+                                }
                             }
                         ?>
                         <div class="row">
