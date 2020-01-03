a41949cf673dc02ea1c65baf9405f6511c39e3d0
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2020-01-03 08:38:21 +0530

    dev

--- console/api/api_service.php
+++ console/api/api_service.php
@@ -13,22 +13,22 @@ if (isset($_GET['o'])) {
     else if ($o == 'service_start') {
         $service_name = $_GET['service_name'];
         echo shell_exec('sudo service '.$service_name.' start');
-        echo 'Server B =>'.$service_name.' Service Starting';
+        echo '<br>Server B =>'.$service_name.' Service Starting';
     }
     else if ($o == 'service_restart') {
         $service_name = $_GET['service_name'];
         echo shell_exec('sudo service '.$service_name.' restart');
-        echo 'Server B =>'.$service_name.' Service Restarting';
+        echo '<br>Server B =>'.$service_name.' Service Restarting';
     }
     else if ($o == 'service_stop') {
         $service_name = $_GET['service_name'];
         echo shell_exec('sudo service '.$service_name.' stop');
-        echo 'Server B =>'.$service_name.' Service Stopping';
+        echo '<br>Server B =>'.$service_name.' Service Stopping';
     }
     else if ($o == 'service_status') {
         $service_name = $_GET['service_name'];
         echo shell_exec('sudo service '.$service_name.' status');
-        echo 'Server B =>'.$service_name.' Service Status';
+        echo '<br>Server B =>'.$service_name.' Service Status';
     }
     else{
         echo 'Server - B No Operation found';
