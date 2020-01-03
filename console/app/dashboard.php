7af750bc54623ee1c9a6d06cc13641058c738667
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2020-01-03 08:22:08 +0530

    dev

--- console/app/dashboard.php
+++ console/app/dashboard.php
@@ -149,8 +149,7 @@
                                     $status_icon='<i class="fas fa-circle text-danger"></i>&nbsp;&nbsp<span>Stopped</span>';
                                     $service_text_each=trim(str_replace('[ - ]',' ',$service_text_each));
                                 }
-                                echo 
-                                '<div class="row">
+                                echo '<div class="row">
                                     <div class="col-xs-3 col-md-3 md-3 pointer">
                                     '.$status_icon.'
                                     <div class="col-xs-6 col-md-6 md-6"><span>'.$service_text_each.' Service<span></div>
@@ -163,46 +162,6 @@
                                         </div>
                                     </div>
                                 </div>';
-                                // if(strpos($service_text_each, '[ + ]') !== false){
-                                //     $status_text='Running';
-                                //     $status_icon='<div class="col-xs-3 col-md-3 md-3 pointer"><i class="fas fa-circle text-success"></i>&nbsp;&nbsp;<span>Running</span></div>';
-                                //     $service_name_text=str_replace('  [ + ]  ','',$service_text_each);
-                                //     echo 
-                                //     '<div class="row">
-                                //     '.$status_icon.'
-                                //     <span>Running</span></div>
-                                //     <div class="col-xs-6 col-md-6 md-6"><span>'.$service_name_text.' Service<span></div>
-                                //     <div class="col-xs-3 col-md-3 md-3">
-                                //         <div>
-                                //             <i class="fas fa-play text-success pointer" title="Start Service"></i> &nbsp; &nbsp;
-                                //             <i class="fas fa-stop text-danger pointer" title="Stop Service"></i> &nbsp; &nbsp;
-                                //             <i class="fas fa-redo-alt text-info pointer" title="Restart Service"></i> &nbsp; &nbsp;
-                                //             <i class="fas fa-info text-primary pointer" title="Service Status"></i> &nbsp; &nbsp;
-                                //         </div>
-                                //     </div>
-                                // </div>
-                                // <hr>';
-                                // }
-                                // else if(strpos($service_text_each, '[ - ]') !== false){
-                                //     $status_text='Stopped';
-                                //     $status_icon='<div class="col-xs-3 col-md-3 md-3 pointer"><i class="fas fa-circle text-danger"></i>&nbsp;&nbsp;<span>Stopped</span></div>';
-                                //     $service_name_text=str_replace('  [ - ]  ','',$i);
-                                //     echo 
-                                //     '<div class="row">
-                                //     '.$status_icon.'
-                                //     <span>Running</span></div>
-                                //     <div class="col-xs-6 col-md-6 md-6"><span>'.$service_name_text.' Service<span></div>
-                                //     <div class="col-xs-3 col-md-3 md-3">
-                                //         <div>
-                                //             <i class="fas fa-play text-success pointer" title="Start Service"></i> &nbsp; &nbsp;
-                                //             <i class="fas fa-stop text-danger pointer" title="Stop Service"></i> &nbsp; &nbsp;
-                                //             <i class="fas fa-redo-alt text-info pointer" title="Restart Service"></i> &nbsp; &nbsp;
-                                //             <i class="fas fa-info text-primary pointer" title="Service Status"></i> &nbsp; &nbsp;
-                                //         </div>
-                                //     </div>
-                                // </div>
-                                // <hr>';
-                                //}
                             }
                         ?>
                         <div class="row">
