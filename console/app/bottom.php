9831b4b7a6f5d807723c02a851a0dc7574a1d352
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2020-01-04 22:29:20 +0530

    dev

--- console/app/bottom.php
+++ console/app/bottom.php
@@ -1,40 +1,40 @@
 </div>
-            <!-- End of Main Content -->
+<!-- End of Main Content -->
 
-            <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright (c) 2020 Server B - open source initiative | Last Updated @ <span style="text-transform: uppercase;"><?php echo shell_exec("bash /var/www/server-b/system/scripts/service.sh getKey update_date"); ?></span></span>
-                    </div>
-                </div>
-            </footer>
-            <!-- End of Footer -->
-
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright (c) 2020 Server B - open source initiative | Last Updated @ <span style="text-transform: uppercase;"><?php echo shell_exec("bash /var/www/server-b/system/scripts/service.sh getKey update_date"); ?></span></span>
         </div>
-        <!-- End of Content Wrapper -->
-
     </div>
-    <!-- End of Page Wrapper -->
+</footer>
+<!-- End of Footer -->
 
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
+</div>
+<!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
 
 
-    <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
-    <!-- Page level plugins -->
-    <script src="vendor/chart.js/Chart.min.js"></script>
-    <!-- Page level custom scripts -->
-    <script src="js/chart-server-b.js"></script>
-    <script src="js/server-b.js"></script>
+<!-- Bootstrap core JavaScript-->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- Core plugin JavaScript-->
+<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+<!-- Custom scripts for all pages-->
+<script src="js/sb-admin-2.min.js"></script>
+<!-- Page level plugins -->
+<script src="vendor/chart.js/Chart.min.js"></script>
+<!-- Page level custom scripts -->
+<script src="js/chart-server-b.js"></script>
+<script src="js/server-b.js"></script>
 
 </body>
 
