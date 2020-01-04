ca39fa53c60a809864f1d81148fb3660ef289755
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2020-01-04 20:49:25 +0530

    dev

--- console/app/top.php
+++ console/app/top.php
@@ -83,8 +83,8 @@ $api_link='http://'.$public_ip.':'.$server_b_port.'/api/';
                 <div class="sidebar-brand-text mx-2">Server B</div>                
             </a>
             <a class="sidebar-brand d-flex align-items-center justify-content-center p-0" style="margin-top:-20px;line-height:12px;" href="dashboard.php">
-                <img src="<?php echo $app_link.'/img/Opensource.svg'; ?>">
-                <div class="sidebar-brand-text mx-2"><small style="font-size: 8px;">Simple Server Management</small><br><small style="font-size: 8px;">Open Source Initiative</small></div>                
+                <img src="<?php echo $app_link.'/img/Opensource.svg'; ?>" style="width:40px;">
+                <div class="sidebar-brand-text mx-2" style="margin-top:-10px;"><small style="font-size: 8px;">Simple Server Management</small><br><small style="font-size: 8px;">Open Source Initiative</small></div>                
             </a>
             <!-- Divider -->
             <hr class="sidebar-divider my-0">
