9053c723d504f4031d6dc27652014b294fc507fd
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2020-01-04 19:46:01 +0530

    dev

--- console/app/js/server-b.js
+++ console/app/js/server-b.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
-    $("body").click(function() {
-
+    $(".serviceBtn").on("click", function() {
+        var url = $(this).attr('link');
+        var preText = $(this).attr('pre');
+        var postText = $(this).attr('pre');
+        var c = confirm(preText);
+        if (c) {
+            $.ajax({
+                url,
+                type: 'GET',
+                dataType: 'text',
+                success: function(data) {
+                    alert(postText);
+                },
+                error: function(request, error) {
+                    alert(error);
+                }
+            });
+        }
     });
 });
