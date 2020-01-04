9e2c6175999829cea66a3589d9bb536acd3ae0cc
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2020-01-04 20:01:11 +0530

    dev

--- console/app/dashboard.php
+++ console/app/dashboard.php
@@ -158,10 +158,10 @@
                                     <div class="col-xs-6 col-md-6 md-6"><span>'.$service_text_each.' Service<span></div>
                                     <div class="col-xs-3 col-md-3 md-3">
                                         <div>
-                                            <a href="#" class="serviceBtn" pre="Starting Service: '.$service_text_each.'" link="'.$api_link .'api_service.php?o=service_start&service_name='.$service_text_each.'"><i class="fas fa-play text-success pointer" title="Start Service"></i></a> &nbsp; &nbsp;
-                                            <a href="#" class="serviceBtn" pre="Stopping Service: '.$service_text_each.'" link="'.$api_link .'api_service.php?o=service_stop&service_name='.$service_text_each.'"><i class="fas fa-stop text-danger pointer" title="Stop Service"></i></a> &nbsp; &nbsp;
-                                            <a href="#" class="serviceBtn" pre="Restarting Service: '.$service_text_each.'" link="'.$api_link .'api_service.php?o=service_restart&service_name='.$service_text_each.'"><i class="fas fa-redo-alt text-info pointer" title="Restart Service"></i></a> &nbsp; &nbsp;
-                                            <a href="#" class="serviceBtn" reload="false" pre="Fetching Service Status: '.$service_text_each.'" link="'.$api_link .'api_service.php?o=service_status&service_name='.$service_text_each.'"><i class="fas fa-info text-primary pointer" title="Service Status"></i></a> &nbsp; &nbsp;
+                                            <a href="#" class="serviceBtn" reload=true pre="Starting Service: '.$service_text_each.'" link="'.$api_link .'api_service.php?o=service_start&service_name='.$service_text_each.'"><i class="fas fa-play text-success pointer" title="Start Service"></i></a> &nbsp; &nbsp;
+                                            <a href="#" class="serviceBtn" reload=true pre="Stopping Service: '.$service_text_each.'" link="'.$api_link .'api_service.php?o=service_stop&service_name='.$service_text_each.'"><i class="fas fa-stop text-danger pointer" title="Stop Service"></i></a> &nbsp; &nbsp;
+                                            <a href="#" class="serviceBtn" reload=true pre="Restarting Service: '.$service_text_each.'" link="'.$api_link .'api_service.php?o=service_restart&service_name='.$service_text_each.'"><i class="fas fa-redo-alt text-info pointer" title="Restart Service"></i></a> &nbsp; &nbsp;
+                                            <a href="#" class="serviceBtn" reload=false pre="Fetching Service Status: '.$service_text_each.'" link="'.$api_link .'api_service.php?o=service_status&service_name='.$service_text_each.'"><i class="fas fa-info text-primary pointer" title="Service Status"></i></a> &nbsp; &nbsp;
                                         </div>
                                     </div>
                                 </div>
