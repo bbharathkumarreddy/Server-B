4305144b51d42ee35f0ea941ea5b0f22fb44ce30
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2020-01-06 00:03:46 +0530

    dev

--- console/app/cronkeep-master/src/index.php
+++ console/app/cronkeep-master/src/index.php
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-require_once 'vendor/autoload.php';
+require_once '../vendor/autoload.php';
 use \models\Crontab;
 use \models\Crontab\Exception;
 use \models\SystemUser;
