6c55d932db997a51a85635cef05abf29537c6540
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2020-01-07 22:59:12 +0530

    dev

--- console/app/top.php
+++ console/app/top.php
@@ -8,6 +8,7 @@ $service='bash /var/www/server-b/system/scripts/service.sh';
 $getKey = 'bash /var/www/server-b/system/scripts/service.sh getKey';
 $shell_in_box_access_port=shell_exec("bash /var/www/server-b/system/scripts/service.sh getKey shell_in_box_access_port");
 $app_link='http://'.$public_ip.':'.$server_b_port.'/app/';
+$app_file_manager_link='http://'.$public_ip.':'.$server_b_port.'/app/file-manager.php?';
 $api_link='http://'.$public_ip.':'.$server_b_port.'/api/';
 $server_b_data='/var/www/server-b-data';
 ?>
