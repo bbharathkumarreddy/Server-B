f337d53140a53752d1f5558f9b2795fc6f98f8db
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2020-01-07 23:50:54 +0530

    dev

--- console/api/api_service.php
+++ console/api/api_service.php
@@ -66,7 +66,7 @@ if (isset($_GET['o'])) {
     else if ($o == 'publish_cron_file') {
         if($_GET['status'] == true) $status = 'enable';
         else if($_GET['status'] == false) $status = 'disable';
-        echo shell_exec('ufw --force '+$status);
+        echo shell_exec('sudo ufw --force '+$status);
         echo '\n Server B =>ufw firewall Successully';
     }
     else{
