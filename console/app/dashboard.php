978419102999a98fa3b59ce7ef04b949545e3ed3
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2020-01-09 22:22:37 +0530

    dev

--- console/app/dashboard.php
+++ console/app/dashboard.php
@@ -5,6 +5,10 @@
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+        <a href="<?php echo $app_link; ?>file-manager.php" class="d-none d-sm-inline-block btn btn-sm btn-info shadow-sm"><i class="fa fa-file-code fa-sm text-white-50"></i> File Manager</a>
+    
+        <a href="<?php echo $app_link; ?>ssh.php" class="d-none d-sm-inline-block btn btn-sm btn-success shadow-sm"><i class="fas fa fa-greater-than fa-sm text-white-50"></i> SSH</a>
+    
     </div>
 
     <!-- Content Row -->
