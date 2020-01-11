218faff34e21d843372c70961a9e17c2e52ff9b7
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2020-01-11 12:30:10 +0530

    dev

--- console/app/commands-help.php
+++ console/app/commands-help.php
@@ -6,7 +6,12 @@
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Commands Help</h1>
     </div>
-
+    <div class="row mb-4">
+        <div class="col-xl-12 col-md-12 mb-12 fr">
+            <a href="<?php echo $app_link; ?>file-manager.php" class="btn btn-sm btn-info shadow-sm fr"><i class="fa fa-file-code fa-sm text-white-50"></i> File Manager</a>
+            <a href="<?php echo $app_link; ?>ssh.php" class="btn btn-sm btn-success shadow-sm fr mr-10"><i class="fas fa fa-greater-than fa-sm text-white-50"></i> SSH</a>
+        </div>
+    </div>
     <div class="row">
         <div class="col-xl-12 col-md-12 mb-12">
             <div class="card shadow mb-12" id="services">
