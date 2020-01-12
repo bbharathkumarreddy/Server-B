bf5409666509eac4b9764d66d3ad12731658b084
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2020-01-12 14:11:54 +0530

    dev

--- console/app/app-store.php
+++ console/app/app-store.php
@@ -21,20 +21,20 @@
                 <div class="card-body row ma">
                     <?php
                     $list =  array(
-                        "nginx" => array("name" => "nginx", "img" => "nginx.png", "width" => "55", "css" => "app-card-success", "status" => true),
-                        "php" => array("name" => "php", "img" => "php.png", "width" => "55", "css" => "app-card-success", "status" => true),
+                        "nginx" => array("name" => "nginx", "img" => "nginx.png", "width" => "55", "status" => true),
+                        "php" => array("name" => "php", "img" => "php.png", "width" => "55", "status" => true),
                         "apache" => array("name" => "apache", "img" => "apache.png", "width" => "32", "css" => "app-card-danger", "status" => false),
-                        "nodejs" => array("name" => "nodejs", "img" => "nodejs.png", "width" => "42", "css" => "app-card-success", "status" => true),
-                        "python" => array("name" => "python", "img" => "python.png", "width" => "50", "css" => "app-card-success", "status" => true),
-                        "golang" => array("name" => "golang", "img" => "golang.png", "width" => "48", "css" => "app-card-success", "status" => true),
-                        "mysql" => array("name" => "mysql", "img" => "mysql.png", "width" => "55", "css" => "app-card-success", "status" => true),
-                        "postgres" => array("name" => "postgres", "img" => "postgres.png", "width" => "31", "css" => "app-card-success", "status" => true),
-                        "mongodb" => array("name" => "mongodb", "img" => "mongodb.png", "width" => "44", "css" => "app-card-success", "status" => true),
-                        "elasticsearch" => array("name" => "elasticsearch", "img" => "elasticsearch.png", "width" => "29", "css" => "app-card-success", "status" => true),
-                        "redis" => array("name" => "redis", "img" => "redis.png", "width" => "38", "css" => "app-card-success", "status" => true),
-                        "npm" => array("name" => "npm", "img" => "npm.png", "width" => "54", "css" => "app-card-success", "status" => true),
-                        "jupyter" => array("name" => "jupyter", "img" => "jupyter.png", "width" => "44", "css" => "app-card-success", "status" => true),
-                        "shellbox" => array("name" => "shellbox", "img" => "shellinabox.png", "width" => "36", "css" => "app-card-success", "status" => true)
+                        "nodejs" => array("name" => "nodejs", "img" => "nodejs.png", "width" => "42", "status" => true),
+                        "python" => array("name" => "python", "img" => "python.png", "width" => "50", "status" => true),
+                        "golang" => array("name" => "golang", "img" => "golang.png", "width" => "48", "status" => true),
+                        "mysql" => array("name" => "mysql", "img" => "mysql.png", "width" => "55", "status" => true),
+                        "postgres" => array("name" => "postgres", "img" => "postgres.png", "width" => "31", "status" => true),
+                        "mongodb" => array("name" => "mongodb", "img" => "mongodb.png", "width" => "44", "status" => true),
+                        "elasticsearch" => array("name" => "elasticsearch", "img" => "elasticsearch.png", "width" => "29", "status" => true),
+                        "redis" => array("name" => "redis", "img" => "redis.png", "width" => "38", "status" => true),
+                        "npm" => array("name" => "npm", "img" => "npm.png", "width" => "54", "status" => true),
+                        "jupyter" => array("name" => "jupyter", "img" => "jupyter.png", "width" => "44", "status" => true),
+                        "shellbox" => array("name" => "shellbox", "img" => "shellinabox.png", "width" => "36", "status" => true)
                     );
                     $base_list =  array("nginx","php","apache","nodejs","python","golang","mysql","postgres","mongodb","elasticsearch","redis","npm","jupyter","shellbox");
 
@@ -42,13 +42,10 @@
                         if($each == "") { echo '<hr style="width: 100%;">'; continue; }
                         
                         $p = shell_exec('dpkg --get-selections | grep '.$list[$each]['name']);
-                        print_r($p);echo '<br>';
-                        echo 'value=>'.strpos($p, $list[$each]['name']);
                         $css = 'app-card-danger';
-                        if (trim(strpos($p, $list[$each]['name'])) != '') { $css = 'app-card-success'; }
-
-                        if($list[$each]['name'] == 'elasticsearch') { $list[$each]['name'] = 'Els.Search'; }
-                        echo '<div class="'.$list[$each]['css'].' card">
+                        if (trim(strpos($p, $list[$each]['name'])) != '') $css = 'app-card-success';
+                        if($list[$each]['name'] == 'elasticsearch') $list[$each]['name'] = 'Els.Search';
+                        echo '<div class="'.$css.' card">
                         <div class="card-body p-05">
                             <div class="row no-gutters align-items-center">
                                 <div class="col-auto">
