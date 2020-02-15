b95d2ff6216f8d0bb1c52ddfd63444482fce875f
bbharathkumarreddy96 <bbharathkumarreddy96@gmail.com>
2020-02-15 12:44:28 +0530

    Https Update

--- console/app/file-manager.php
+++ console/app/file-manager.php
@@ -13,7 +13,7 @@
     </div>
     <div class="row">
         <div class="col-xl-12 col-md-12 mb-12">
-            <iframe style="width:100%;height:1000px;" src="<?php echo 'http://' . $public_ip . ':' . $server_b_port . '/app/embed-filemanager.php?'.$link; ?>"></iframe>
+            <iframe style="width:100%;height:1000px;" src="<?php echo 'https://' . $public_ip . ':' . $server_b_port . '/app/embed-filemanager.php?'.$link; ?>"></iframe>
         </div>
     </div>
 
